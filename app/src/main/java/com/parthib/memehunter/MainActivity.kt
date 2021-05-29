92ed9d7d5134a29bbcd7cb434017d165957c6c77
lucy2512 <mail4parthib@gmail.com>
2021-05-29 20:54:53 +0530

    :package:Volley Library Integration

--- app/src/main/java/com/parthib/memehunter/MainActivity.kt
+++ app/src/main/java/com/parthib/memehunter/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.parthib.memehunter
 
+import android.app.DownloadManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadmeme()
+    }
+
+    private fun loadmeme(){
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = " https://meme-api.herokuapp.com/gimme"
+
+// Request a string response from the provided URL.
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+        Response.Listener { response ->
+            val url = response.getString("url")
+        },
+        Response.ErrorListener { error ->
+            // TODO: Handle error
+        }
+        )
+
+// Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest)
+    }
+
+    fun shareMeme(view: View) {}
+    fun nextMeme(view: View) {
     }
 }
